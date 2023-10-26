de4393e97de8c16105b224e69b505e9dcbda4618
sophielyall <sophie.lyall@outlook.com>
2023-10-26 16:16:23 +1100

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+/**
+*A simple arithmetic calculator that adds, subtracts, muliply,
+*divide and the modulo operator on two integers.
+*/
+public class Calculator{
+  public Calculator(){
+
+  }
+
+  public int add(int a, int b)
+  {
+    return a+b;
+  }
+
+  public int subtract(int a, int b)
+  {
+    return a - b;
+  }
+
+  public int multiply(int a, int b){
+    return a * b;
+  }
+
+  public int divide(int a, int b)
+  {
+      return a/b;
+  }
+
+  public int modulo(int a, int b){
+    return a%b;
+  }
+
+  public static void main(String[] args)
+  {
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5,7));
+    System.out.println(myCalculator.subtract(45,11));
+    System.out.println(myCalculator.multiply(9,6));
+    System.out.println(myCalculator.divide(96,12));
+    System.out.println(myCalculator.modulo(16,3));
+  }
+}
